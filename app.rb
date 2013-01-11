16826ef11e1b0a9badd4d4ec5fe9a535f7427b9c
st <st@st-netbook.(none)>
2013-01-11 04:29:57 +0100

    Add so many stuff

--- app.rb
+++ app.rb
@@ -1,23 +1,25 @@
 require "sinatra"
 require "erb"
-require "mysql"
+require "sqlite3"
 
+set :public_folder, Proc.new { File.join(root, "assets") }
 
 # obrazek [id, nazwa, opis, adres linku, adres miniaturki]
 configure do
-	@db = Mysql.new(#credentials)
-	enable :sessions
+	#@db = Mysql.new(#credentials)
+	#enable :sessions
 end
 
 get '/' do
-	erb :index
+	erb :index2
 end
+
 
 get '/photo/:id' do 
 
-	id = :id
-	@img = @db.query("SELECT * where id=#{id}")
+#	id = :id
+#	@img = @db.query("SELECT * where id=#{id}")
 	
-	erb :photo # a moze zapodaj jsonem? i zmontujmy to w js?
+#	erb :photo # a moze zapodaj jsonem? i zmontujmy to w js?
 end
 
