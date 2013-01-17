921693efcc889ba75cc7e268531baff5cdc52b8c
st <st@st-lap.(none)>
2013-01-17 12:59:27 +0100

    proba

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,25 +1,24 @@
 (function( $ ){
 
-  $.fn.Gallery = function() {
-  
-    // there's no need to do $(this) because
-    // "this" is already a jquery object
 
-    // $(this) would be the same as $($('#element'));
-
-
-    this.fadeIn('normal', function(){
-
-      // the this keyword is a DOM element
-
-    });
-
-    var methods = {
+	var methods = {
     init : function( ) { 
       $.get()
     },
     update : function( ) {
       // TODO
     }
+
+  $.fn.Gallery = function( method ) {
+    
+    if ( methods[method] ) {
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }  
+
+    
   };
 })( jQuery );
